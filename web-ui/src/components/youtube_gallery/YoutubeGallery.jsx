3a1849c5f1ced5c010d1db3611e249b21ba7b184
Pritam Prasad <pritam.prasd@gmail.com>
2021-07-16 00:20:35 +0530

    filter by tag

--- web-ui/src/components/youtube_gallery/YoutubeGallery.jsx
+++ web-ui/src/components/youtube_gallery/YoutubeGallery.jsx
@@ -1,15 +1,65 @@
+import { useEffect, useState } from 'react'
 import './YoutubeGallery.css'
 import ytchannels from "./ytchannels.json"
 
+
+const tagToColorMap = {
+    "science": "bacc13",
+    "math": "f94144",
+    "cryptography": "f3722c",
+    "animation": "f9844a",
+    "physics": "f9c74f",
+    "psychology": "90be6d",
+    "history": "43aa8b",
+    "human": "4d908e",
+    "stories": "577590",
+    "living": "277da1",
+    "travel": "f3722c",
+    "culture": "f3722c",
+    "world": "f3722c",
+    "biotech": "f3722c",
+    "tech": "ffcdb2",
+    "code": "e5989b",
+}
+
 export default function YoutubeGallery() {
-    return (
-        <div className="container">
-            {
-                ytchannels.map(channel => <ChannelTile key={channel.name} channel={channel}/>)
+
+    const[selectedTag, setSelectedTag] = useState("all")
+    const[filteredChanels, setFilteredChanels] = useState(ytchannels)
+
+    useEffect(
+        () => {
+            if(selectedTag !== "all"){
+                setFilteredChanels(ytchannels.filter(c => c.tags.indexOf(selectedTag) > -1));
+            } else{
+                setFilteredChanels(ytchannels);
             }
+        }
+    ,[selectedTag])
+    
+    const handleOnTagSelect = (e) => {
+        setSelectedTag(e.target.value)
+        console.log(e.target.value)
+    }
+
+    return (
+        <div>
+            <label for="tags">Filter by tag: </label>
+            <select name="tags" id="tags-select-id" onChange={handleOnTagSelect}>
+                <option value="all" selected>all</option>
+                {
+                    Object.keys(tagToColorMap).map(tag => <option value={tag}>{tag}</option>)
+                }
+            </select>
+            <div className="container">
+                {
+                    filteredChanels.map(channel => <ChannelTile key={channel.name} channel={channel}/>)
+                }
+            </div>
         </div>
     )
 }
+
 
 
 const ChannelTile = (props) => {
@@ -38,24 +88,6 @@ const ChannelTile = (props) => {
 }
 
 function createTag(t) {
-    const tagToColorMap = {
-        "science": "bacc13",
-        "math": "f94144",
-        "cryptography": "f3722c",
-        "animation": "f9844a",
-        "physics": "f9c74f",
-        "psychology": "90be6d",
-        "history": "43aa8b",
-        "human": "4d908e",
-        "stories": "577590",
-        "living": "277da1",
-        "travel": "f3722c",
-        "culture": "f3722c",
-        "world": "f3722c",
-        "biotech": "f3722c",
-        "tech": "ffcdb2",
-        "code": "e5989b",
-    }
     return getTag(tagToColorMap[t])
 
     function getTag(color) {
